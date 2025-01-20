6d2eb4f589b50739eaa61cfc92b567b47b0a0ae7
HaoYeRen233 <2708044832@qq.com>
2025-01-20 15:59:56 +0800

    Update auth.go

--- pkg/model/auth.go
+++ pkg/model/auth.go
@@ -9,15 +9,19 @@ type LoginResponseDTO struct {
 	BaseResp
 	Token string  `json:"token"`
 	User  UserDTO `json:"user"`
+	Error error
 }
 
 type RegisterRequestDTO struct {
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
+	UserName        string `json:"user_name"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
 }
 
 type RegisterResponseDTO struct {
 	BaseResp
 	Token string  `json:"token"`
 	User  UserDTO `json:"user"`
+	Error error
 }
